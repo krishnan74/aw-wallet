f8c9e4911f9a978728c876d65566dae4c698e63d
krishnan74 <divyakrishnan.r74@gmail.com>
2024-07-21 22:43:04 +0530

    fix vercel deployment debug

--- app/components/DropDown.jsx
+++ app/components/DropDown.jsx
@@ -1,4 +1,3 @@
-"use client";
 import React from "react";
 import { useState } from "react";
 import { FaChevronDown } from "react-icons/fa";
@@ -8,37 +7,41 @@ const DropDown = ({ links, children }) => {
   const toggle = () => {
     setIsOpen(!isOpen);
   };
+
   return (
-    <div class="relative inline-block text-left">
+    <div className="relative inline-block text-left">
       <div onMouseEnter={toggle} onMouseLeave={toggle}>
         <button
           type="button"
-          class="inline-flex w-full justify-center items-center gap-x-2 rounded-md bg-white px-3 py-2 text-sm  text-gray-900   hover:bg-gray-50"
+          className="inline-flex w-full justify-center items-center gap-x-2 rounded-md bg-white px-3 py-2 text-sm text-gray-900 hover:bg-gray-50"
           id="menu-button"
         >
           {links[0]}
-          <FaChevronDown></FaChevronDown>
+          <FaChevronDown />
         </button>
       </div>
 
       {isOpen && (
         <div
-          class="absolute right-0 z-10 p-y-2  w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
-          tabindex="-1"
+          className="absolute right-0 z-10 p-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+          tabIndex="-1"
           onMouseEnter={() => setIsOpen(true)}
           onMouseLeave={() => setIsOpen(false)}
         >
-          {links.map((link, index) => {
-            if (index === 0) return;
-            return (
+          {links.slice(1).map(
+            (
+              link,
+              index // Use slice(1) to skip the first element which is already rendered
+            ) => (
               <a
+                key={index} // Add key prop here
                 href="#"
                 className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
               >
                 {link}
               </a>
-            );
-          })}
+            )
+          )}
         </div>
       )}
     </div>
--- app/components/FooterLinks.jsx
+++ app/components/FooterLinks.jsx
@@ -5,13 +5,19 @@ const FooterLinks = ({ links }) => {
   return (
     <div className="flex flex-col gap-3">
       {links.map((link, index) => {
-        if (index === 0)
-          return <p className="text-md font-bold mb-3">{link}</p>;
-        return (
-          <Link href={"#"} className="text-sm">
-            {link}
-          </Link>
-        );
+        if (index === 0) {
+          return (
+            <p key={index} className="text-md font-bold mb-3">
+              {link}
+            </p>
+          );
+        } else {
+          return (
+            <Link key={index} href="#">
+              <a className="text-sm">{link}</a>
+            </Link>
+          );
+        }
       })}
     </div>
   );
